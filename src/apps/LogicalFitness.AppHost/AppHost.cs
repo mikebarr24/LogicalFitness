a6200d11dc6e2782228cd7fa3f643d59fc8798c8
Michael <michaelbarr24@hotmail.com>
2025-12-30 21:02:45 +0000

    Posgres Db service added to AppHost

--- src/apps/LogicalFitness.AppHost/AppHost.cs
+++ src/apps/LogicalFitness.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder
+  .AddPostgres("pg-logical-fitness")
+  .WithDataVolume("pg-logical-fitness-data");
+
+var db = postgres.AddDatabase("db-logical-fitness");
+
 builder.Build().Run();
