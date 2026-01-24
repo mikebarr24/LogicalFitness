fe69ccfcb2eddeb1cf166c712e8c5cd530a7c6fc
Michael <michaelbarr24@hotmail.com>
2026-01-24 00:11:11 +0000

    Start on Domain Architecture tests.

---
+++ tests/LogicalFitness.Architecture.Tests/DomainArchitectureTests.cs
@@ -0,0 +1,76 @@
+using NetArchTest.Rules;
+
+namespace LogicalFitness.Architecture.Tests;
+
+public class DomainArchitectureTests
+{
+  private const string DomainNamespace = "Domain";
+  private const string ApplicationNamespace = "Application";
+  private const string InfrastructureNamespace = "Infrastructure";
+  private const string ApiNamespace = "Api";
+
+  [Test]
+  public async Task Domain_Should_Not_HaveDependencyOnOtherProjects()
+  {
+    //Arrange
+    var assembly = typeof(Domain.AssemblyReference).Assembly;
+
+    var otherProjects = new[]
+    {
+      ApplicationNamespace,
+      InfrastructureNamespace,
+      ApiNamespace
+    };
+
+    //Act
+    var result = Types
+      .InAssembly(assembly)
+      .ShouldNot()
+      .HaveDependencyOnAll(otherProjects)
+      .GetResult();
+
+    //Assert
+    await Assert.That(result.IsSuccessful).IsTrue();
+  }
+
+  [Test]
+  public async Task Domain_Should_Not_ReferenceDisallowedTypes()
+  {
+    // Arrange
+    var domainAssembly = typeof(Domain.AssemblyReference).Assembly;
+
+    var disallowedUsings = new[]
+    {
+      "Microsoft",
+      "EntityFrameworkCore",
+      "MediatR"
+    };
+
+    // Act
+    var result = Types
+      .InAssembly(domainAssembly)
+      .ShouldNot()
+      .HaveDependencyOnAll(disallowedUsings)
+      .GetResult();
+
+    // Assert
+    await Assert.That(result.IsSuccessful).IsTrue();
+  }
+
+  [Test]
+  public async Task Domain_Should_Not_ContainAnyDtos()
+  {
+    // Arrange
+    var domainAssembly = typeof(Domain.AssemblyReference).Assembly;
+
+    // Act
+    var result = Types
+      .InAssembly(domainAssembly)
+      .ShouldNot()
+      .HaveNameEndingWith("Dto")
+      .GetResult();
+
+    // Assert
+    await Assert.That(result.IsSuccessful).IsTrue();
+  }
+}
